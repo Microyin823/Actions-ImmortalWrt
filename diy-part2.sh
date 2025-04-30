3a4dc7595fd41839f98c62e8a6dc6a14c71106a0
microyin823 <151486794+Microyin823@users.noreply.github.com>
2025-04-30 12:16:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,11 +18,10 @@ git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/l
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
-find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Replace luci-app-ssr-plus & Depends
 git clone --depth=1 -b master https://github.com/fw876/helloworld
